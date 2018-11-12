d5e26547d8c61756704711bd4eba0a09fc076057
Jeremy Lee <jlee00@stuy.edu>
2018-11-12 11:18:27 -0500

    added AddWord

--- WordSearch.java
+++ WordSearch.java
@@ -1,5 +1,19 @@
+import java.util.*; //random, scanner, arraylist
+import java.io.*; //file, filenotfoundexception
+
 public class WordSearch {
     private char[][]data;
+    //the random seed used to produce this WordSearch
+    private int seed;
+
+    //a random Object to unify your random calls
+    private Random randgen;
+
+    //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
+    private ArrayList<String>wordsToAdd;
+
+    //all words that were successfully added get moved into wordsAdded.
+    private ArrayList<String>wordsAdded;
 
     /**Initialize the grid to the size specified
      *and fill all of the positions with '_'
@@ -109,4 +123,51 @@ public class WordSearch {
       }
       return true;
     }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added in the direction rowIncrement,colIncrement
+     *Words must have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+     *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+     *@return true when: the word is added successfully.
+     *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+     *        OR there are overlapping letters that do not match
+     */
+     private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+       if (rowIncrement == 0 && colIncrement == 0) {
+          return false;
+        }
+        int colI = col;
+        int rowI = row;
+        for (int i=0; i<word.length(); i++){
+          if (rowI < 0 || rowI >= data.length || colI < 0 || colI >= data[rowI].length) {
+            return false;
+          } else if (data[rowI][colI] != '_' && data[rowI][colI] != word.charAt(i)) {
+            return false;
+          }
+          colI += colIncrement;
+          rowI += rowIncrement;
+        }
+
+        colI = col;
+        rowI = row;
+        for (int i=0; i<word.length(); i++){
+          data[rowI][colI] = word.charAt(i);
+          rowI += rowIncrement;
+          colI += colIncrement;
+        }
+        wordsToAdd.remove(word);
+        wordsAdded.add(word);
+        return true;
+     }
+
+     /*[rowIncrement,colIncrement] examples:
+      *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+      *[ 1,0] would add downwards because (row+1), with no col change
+      *[ 0,-1] would add towards the left because (col - 1), with no row change
+      */
 }
